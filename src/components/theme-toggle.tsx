ac9bba30c55f929686fb806623cd1184cc68a9f3
srinivasa pradeep <sspradeep2004@gmail.com>
2026-01-17 12:38:49 +0000

    still its not obvious

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -36,8 +36,8 @@ export function ThemeToggle() {
         className="relative h-[1.2rem] w-[1.2rem] transition-transform duration-700 ease-in-out"
         style={{ transform: `rotate(${rotation}deg)` }}
       >
-        <Sun className="absolute inset-0 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0" />
-        <Moon className="absolute inset-0 h-[1.2rem] w-[1.2rem] -rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Sun className="absolute inset-0 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all duration-700 dark:rotate-90 dark:scale-0" />
+        <Moon className="absolute inset-0 h-[1.2rem] w-[1.2rem] -rotate-90 scale-0 transition-all duration-700 dark:rotate-0 dark:scale-100" />
       </div>
       <span className="sr-only">Toggle theme</span>
     </Button>
